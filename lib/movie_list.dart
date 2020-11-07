2244bec77aa677c1889a354f640395194f474d13
tresbon <vladi.zubkov@gmail.com>
2020-11-07 21:28:49 +0300

    Added movie page

--- lib/movie_list.dart
+++ lib/movie_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'http_helper.dart';
+import 'movie_detail.dart';
 
 class MovieList extends StatefulWidget {
   @override
@@ -10,6 +12,8 @@ class _MovieListState extends State<MovieList> {
   HttpHelper helper;
   int moviesCount;
   List movies;
+  final String iconBase = 'https://image.tmdb.org/t/p/w92/';
+  final String defaultImage = 'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
 
   @override
   void initState() {
@@ -21,15 +25,33 @@ class _MovieListState extends State<MovieList> {
 
   @override
   Widget build(BuildContext context) {
+    NetworkImage image;
     return Scaffold(
         appBar: AppBar(title: Text('Movies'),),
         body: ListView.builder (
             itemCount: (this.moviesCount==null) ? 0 : this.moviesCount,
             itemBuilder: (BuildContext context, int position) {
+              if (movies[position].posterPath != null) {
+                image = NetworkImage(
+                  iconBase + movies[position].posterPath
+                );
+              } else {
+                image = NetworkImage(defaultImage);
+              }
               return Card(
                 color: Colors.white,
                 elevation: 5.0,
                 child: ListTile(
+                  onTap: () {
+                    MaterialPageRoute route = MaterialPageRoute(
+                        builder: (_) => MovieDetail(
+                          movies[position]
+                        ));
+                    Navigator.push(context, route);
+                  },
+                  leading: CircleAvatar(
+                    backgroundImage: image,
+                  ),
                   title: Text(movies[position].title),
                   subtitle: Text('Released: ${movies[position].releaseDate}\n'
                       +  'Rating: ${movies[position].voteAverage.toString()}'
